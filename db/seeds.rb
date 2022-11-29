fa60ddf9e7096cc67d9cd07ad374d9f280dd3af6
Ilias Wardere <iwardere1@gmail.com>
2022-11-29 16:00:49 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+admin_user = User.create({ first_name: "John", last_name: "Doe", email: "test@test.com", password: "123456", handle: "john"})
+event_1 = Event.create({ name: "Adele Tribute", description: "Incredible artist's songs all day long", location: "London", date_details: DateTime.new(2022,11,12,19), handle: "john", price: 35.00, venue: "O2 Brixton", url: "https://www.ticketmaster.co.uk/adele-tickets/artist/1159272", category: "Pop", lng: 51.5072, lat: 0.1276, image_url: "https://media.ticketmaster.co.uk/tm/en-gb/dam/a/551/eecce133-8e2f-4e56-ae45-45909f7a7551_1170241_CUSTOM.jpg" })
