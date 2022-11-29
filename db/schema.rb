ed6ca511eb8a2489f63ee334cba534f609f5c30e
George Hill <georgeophill@outlook.com>
2022-11-29 15:35:44 +0000

    Added Image_url to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_111417) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_153308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_111417) do
     t.float "lat"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "favorite_groups", force: :cascade do |t|
