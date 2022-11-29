6fc0657b9fbde950bdbdb5f255bcb7b82a7ffa3b
Ilias Wardere <iwardere1@gmail.com>
2022-11-29 17:28:59 +0000

    added daisy ui

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/views/pages/home.html.erb'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "light",
+  },
 }
