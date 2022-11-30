f8f629fe59e6e42eb2c3f5348bbd3480dcd3870b
George Hill <georgeophill@outlook.com>
2022-11-30 16:52:12 +0000

    Edits to user index/show and homepade

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :favorites, dependent: :destroy
   has_many :groups
+  has_many :events, through: :favorites
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :handle, presence: true
