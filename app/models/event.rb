ae1937cfd1fae95325362d3731176bb481e0452c
George Hill <georgeophill@outlook.com>
2022-12-06 15:59:57 +0000

    Edits show

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ApplicationRecord
   has_many :favorites, dependent: :destroy
   validates :name, :location, :date_details, :venue, :url, :category, presence: true
 
-  geocoded_by :location
+  geocoded_by :venue
   after_validation :geocode, if: :will_save_change_to_location?
 end
