40ab60799f79efc778f4e32710c6fbdf8629220a
George Hill <georgeophill@outlook.com>
2023-01-04 13:22:08 +0000

    JavaScript practice

---
+++ javascript-practice/hello.js
@@ -0,0 +1,74 @@
+// forEach
+
+// const beatles = ["paul", "john", "greg", "george"];
+// beatles.forEach((beatle) => {
+//   console.log(beatle.toUpperCase());
+// });
+
+
+// If/else statement
+
+// const age = 21;
+
+// if (age >= 21) {
+//   console.log("You can vote!");
+// } else if (age === 17)
+//   console.log('You are nearly able to vote')
+//   else if (age === 18)
+//   console.log('You are nearly able to vote')
+//   else if (age === 19)
+//   console.log('You are nearly able to vote')
+//   else if (age === 20)
+//   console.log('You are nearly able to vote')
+//   else {
+//   console.log("You can't vote!");
+// }
+
+
+
+
+// Ternary Operator
+
+// const raining = false;
+// const accessory = ( raining ? "umbrella" : "sunglasses");
+// console.log(accessory)
+
+
+
+
+
+// Reading/ Setting a property
+
+// const student = {
+//   firstName: "Boris",
+//   lastName: "Johnson"
+// };
+
+// console.log(student.firstName);
+// student.firstName = "George"
+// console.log(student.firstName)
+
+
+// Defining functions ( Arrow Functions )
+
+// const square = (x) => {
+//   return x * x;
+// };
+
+// // or
+
+// const sqaure = x => x * x
+
+// console.log(square(10))
+
+
+// Lets livecode an arrow function and store it into capitalize
+
+// const capitalize = (word) => {
+//   const firstLetter = word[0].toUpperCase();
+//   console.log(firstLetter);
+//   const restOfTheWord = word.substring(1).toLowerCase();
+//   return `${firstLetter}${restOfTheWord}`;
+// }
+
+// console.log(capitalize('Hello'));
---
+++ javascript-practice/hello.rb
@@ -0,0 +1,8 @@
+# beatles = ["paul", "john", "greg"]
+# beatles.each do |beatle|
+#   puts beatle.upcase
+# end
+
+raining = true
+accessory = (raining ? "umbrella" : "sunglasses")
+puts accessory
